2ec307ba3fa02a6d21c27b1425f5fc6c16e7c3c6
Aryan <as2280@srmist.edu.in>
2023-12-08 23:02:24 +0530

    connected to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
-
+mongoose.connect(process.env.MONGO)
+.then(()=>console.log(`Connected to the database`))
+.catch(err => console.error(err));
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
